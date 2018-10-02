7612972b0354b1155a542ab9309a22fafec42ae0
PeriodicallyMe <43706783+PeriodicallyMe@users.noreply.github.com>
2018-10-02 17:50:06 -0500

    Update rpm.c

--- rpm.c
+++ rpm.c
@@ -4,22 +4,15 @@
 task main()
 {
 	SensorValue[enc] = 0;
-
-	//Sense RPM
-	//float lastPos = 0;
-
+	
 	while()
 	{
 		SensorValue[enc] = 0;
-		motor[port2] = 127;
-		motor[port3] = -127;
 
 		wait1Msec(100);
 
 		float curr = SensorValue[enc];
 		float vel = (curr / 1.66667) * 5;
-		//float vel = (curr) / (0.000166667);
-		//float lastPos = SensorValue[enc];
 		writeDebugStreamLine("%f", vel);
 	}
 }
